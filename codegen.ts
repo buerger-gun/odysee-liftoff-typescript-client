83f80941dc65e9218617acb763f2e7df0f594819
elias dodel <elias.dodel@varmeco.de>
2026-02-02 11:40:51 +0100

    schema url changed

--- codegen.ts
+++ codegen.ts
@@ -1,14 +1,14 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "http://localhost:4000",
+  schema: "https://odysee-liftoff-typescript-server-production.up.railway.app/",
   documents: ["src/**/*.tsx"],
   generates: {
     "./src/__generated__/": {
       preset: "client",
       presetConfig: {
-        gqlTagName: "gql"
-      }
+        gqlTagName: "gql",
+      },
     },
     "./src/__generated__/types.ts": {
       plugins: ["typescript", "typescript-operations"],
